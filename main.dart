97724e407006dc2576f2a37d14987596f1cf6dbc
N1k1ta-a <malys465@gmail.com>
2025-10-09 13:53:33 +0300

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,149 +1,53 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main(List<String> args) {
+  runApp(MaterialApp(home: MyWidget()));
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Bank Card',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyWidget(),
-    );
-  }
-}
-
-class MyWidget extends StatelessWidget {
+class MyWidget extends StatefulWidget {
   const MyWidget({super.key});
 
   @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  double balance = 1_000;
+  bool _isClicked = false;
+
+  void _updatebalance(double amount){
+    balance += amount;
+  }
+
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white, // фон
+    return  Scaffold(
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: SizedBox(
-            width: 300,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(16),
-                boxShadow: [
-                  BoxShadow( // контур карточки
-                    color: Colors.grey,
-                    blurRadius: 10,
-                    
-                  ),
-                ],
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                  // заголовок счета моего
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text(
-                          "Мой счет",
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.cyan,
-                          ),
-                        ),
-                        Container(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 8, vertical: 4),
-                          decoration: BoxDecoration(
-                            color: Colors.cyan[100],
-                            borderRadius: BorderRadius.circular(4),
-                          ),
-                          child: const Text(
-                            "USD",
-                        
-                          ),
-                        ),
-                      ],
-                    ),
-                    
-                    const SizedBox(height: 16),
-                    
-                    
-                    const Text(
-                      "\$10000",
-                      style: TextStyle(
-                        fontSize: 28,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
-                    ),
-                    
-                    const SizedBox(height: 8),
-                    
-                    
-                    Row(
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 6, vertical: 2),
-                          decoration: BoxDecoration(
-                            color: Colors.green[50],
-                            borderRadius: BorderRadius.circular(4),
-                          ),
-                          child: Text(
-                            "+12.4",
-                            style: TextStyle(
-                              fontSize: 12,
-                              fontWeight: FontWeight.w500,
-                              color: Colors.green[700],
-                            ),
-                          ),
-                        ),
-                        const SizedBox(width: 6),
-                        const Text(
-                          "На этой неделе",
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: Colors.black,
-                          ),
-                        ),
-                      ],
-                    ),
-                    
-                    const SizedBox(height: 20),
-                    Container(
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                        color: Colors.cyan,
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      child: const Padding(
-                        padding: EdgeInsets.symmetric(vertical: 12),
-                        child: Text(
-                          "Пополнить",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w500,
-                            fontSize: 16,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Баланс: \$$balance"),
+            Image.network("https://m.media-amazon.com/images/I/617a-qtfETL._AC_UY1100_.jpg"),
+            Text("Dimond Ring #6666", style: TextStyle(
+              fontSize: 24
+            ),),
+            SizedBox(height: 10,),
+            Text("Price"),
+            SizedBox(height: 10,),
+            Text("800 ton", style: TextStyle(
+              fontSize: 24
+            ),),
+            SizedBox(
+              width: double.infinity,
+              child: FilledButton(onPressed: (){
+                setState(() {
+                  _isClicked = !_isClicked;
+                  _isClicked ? _updatebalance(-400) :
+                  _updatebalance(400);
+                });
+              }, child: Text(_isClicked ? "Продать": "Купить"))),
+          ],
         ),
       ),
     );
